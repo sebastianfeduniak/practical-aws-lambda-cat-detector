bbb66da3bc38e2e4f9ac5978f29f081c49160437
Sebastian Feduniak <sebastian.feduniak@pattern-match.com>
2019-08-05 11:37:01 +0200

    Using static config

--- cat-rekognition/src/config.js
+++ cat-rekognition/src/config.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = () => {
-    var img_repo = process.env.BUCKET || 'serverless-cat-detector-img-repo';
-    var results_table = process.env.TABLE || 'ServerlessCatDetectorStatus';
+    var img_repo = process.env.BUCKET || 'serverless-cat-detector-img-repo-sigma';
+    var results_table = process.env.TABLE || 'ServerlessCatDetectorStatusSigma';
 
     return {
         serverless_cat_detector_img_repo: img_repo,
